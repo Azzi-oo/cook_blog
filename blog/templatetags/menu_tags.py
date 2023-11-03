2448aef960d907469d4a0527ba486b7c103d51e3
Azzi-oo <azatdjon@yandex.ru>
2023-11-04 01:23:54 +0500

    add finish templatetags

--- blog/templatetags/menu_tags.py
+++ blog/templatetags/menu_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Category
+from blog.models import Category, Post
 
 
 register = template.Library()
@@ -10,7 +10,13 @@ register = template.Library()
 #     return 
 
 
-@register.inclusion_tag('blog/top_menu.html')
+@register.inclusion_tag('blog/include/tags/top_menu.html')
 def get_categories():
-    category = Category.objects.order_by('name')
-    return {"list": category}
+    category = Category.objects.all()
+    return {"list_category": category}
+
+
+@register.inclusion_tag('blog/include/tags/recipes_tag.html')
+def get_last_posts():
+    posts = Post.objects.select_related('category').order_by('-id')[:5]
+    return {"list_last_post": posts}
